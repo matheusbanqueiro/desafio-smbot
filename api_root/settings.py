914a7958a741a8085823b3d1e99b014b66b6d4c7
matheusbanqueiro <mbanqueirolima@gmail.com>
2024-09-13 21:58:44 -0300

    Style:new port redis

--- api_root/settings.py
+++ api_root/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://127.0.0.1:6379",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
